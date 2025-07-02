1000277c71b766449a2c1adfd171078e1d2cece0
gourav2707 <gouravvishwakarma8161@gmail.com>
2025-07-02 14:55:29 +0530

    Update ProjectCard.js

--- fullstackapp/src/components/ProjectCard.js
+++ fullstackapp/src/components/ProjectCard.js
@@ -5,7 +5,7 @@ import ApiConfig from '../ApiConfig';
 function ProjectCard({ name, description, image }) {
   return (
     <div className="card">
-      <img src={ApiConfig.UPLOAD`/${image}`} alt={name} className="card-img" />
+      <img src={`${ApiConfig.UPLOAD}/${image}`} alt={name} className="card-img" />
       <h3>{name}</h3>
       <p>{description}</p>
       <button className="read-more-btn" disabled>Read More</button>
