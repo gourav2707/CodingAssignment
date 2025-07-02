6dc65e83567b2bd9b8cfe03ec62fbf6504950327
gourav2707 <gouravvishwakarma8161@gmail.com>
2025-07-02 14:56:34 +0530

    Update ClientCard.js

--- fullstackapp/src/components/ClientCard.js
+++ fullstackapp/src/components/ClientCard.js
@@ -5,7 +5,7 @@ import ApiConfig from '../ApiConfig';
 function ClientCard({ name, description, designation, image }) {
   return (
     <div className="card">
-      <img src={ ApiConfig.UPLOAD`/${image}`} alt={name} className="client-img" />
+      <img src={`${ApiConfig.UPLOAD}/${image}`} alt={name} className="client-img" />
       <p>"{description}"</p>
       <h4>{name}</h4>
       <small>{designation}</small>
